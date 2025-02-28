66d2502210b26a1fd38db4888cad1024859290e1
Sayan Shil <sayansil2000@gmail.com>
2025-02-28 14:27:28 +0530

    Mini Exam With JDBC, Oracle

---
+++ src/Student/Student_Exam.java
@@ -0,0 +1,11 @@
+package Student;
+
+public class Student_Exam {
+    public static String ResultID;
+    public static void main(String[] args) {
+        System.out.println("Enter Result ID -> ");
+        ResultID = new java.util.Scanner(System.in).nextLine();
+
+        System.out.println("Thank You For Exam");
+    }
+}
